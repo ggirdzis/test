97eab462910c3f77d1c021bb11de5d8d7ee9ccca
ggirdzis <ggirdzis@uvm.edu>
2015-09-16 19:30:12 -0400

    first commit

---
+++ header.php
@@ -0,0 +1,12 @@
+<!-- %%%%%%%%%%%%%%%%%%%%%%     Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
+
+<header>
+    <figure class="float_left small">
+            <img alt="An image of a sunset in Burlington,VT." src="images/sunset.jpg">
+            <figcaption>Beautiful Burlington Bay.</figcaption>
+    </figure>
+    <h1>UVM Classes</h1>
+    <h2>Find Your Classes Today!</h2>
+</header>
+
+<!-- %%%%%%%%%%%%%%%%%%%%% Ends Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
