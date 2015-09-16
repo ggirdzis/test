fc02b4af6b46f6717f27ff98b8c1dab87d44760d
ggirdzis <ggirdzis@uvm.edu>
2015-09-16 19:35:55 -0400

    second commit

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!-- @@@@@@@@@@@@@@@@@@@@@@     Footer            @@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+<footer>
+    <p>Web site designed by Gretchen E. Girdzis.</p>
+</footer>
+<!-- @@@@@@@@@@@@@@@@@@@@ Ends Footer             @@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+
+</body>
+</html>
